98d54016d92a3c7376786ac14a0201399b7b58b1
vranghel <150031559+vranghel@users.noreply.github.com>
2024-11-04 16:15:41 +0100

    Update advanced_form.py
    update model list

--- infinite_bookshelf/ui/components/advanced_form.py
+++ infinite_bookshelf/ui/components/advanced_form.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-MODEL_LIST = ["llama3-70b-8192", "llama3-8b-8192", "gemma2-9b-it"]
+MODEL_LIST = ["llama-3.2-1b-preview","llama-3.2-3b-preview","llama-3.2-11b-vision-preview","llama-3.2-90b-vision-preview"]
 
 
 def render_advanced_groq_form(on_submit, button_disabled=False, button_text="Generate"):
